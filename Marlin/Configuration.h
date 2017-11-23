b79d275ebebeb8c777ec343010a490222fc1c78d
Victor Martinez <knoopx@gmail.com>
2017-11-24 00:25:29 +0100

    Disable custom machine name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -124,7 +124,7 @@
 
 // Optional custom name for your RepStrap or other custom machine
 // Displayed in the LCD "Ready" message
-#define CUSTOM_MACHINE_NAME "CTC PRUSA I3 PRO B"
+// #define CUSTOM_MACHINE_NAME "CTC PRUSA I3 PRO B"
 
 // Define this to set a unique identifier for this printer, (Used by some programs to differentiate between machines)
 // You can use an online service to generate a random UUID. (eg http://www.uuidgenerator.net/version4)
