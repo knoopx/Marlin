4525eda0b0f1a0319d9512d6feb6d96348d752ed
Victor Martinez <knoopx@gmail.com>
2017-11-24 00:44:35 +0100

    Adjusted extruder steps/mm

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -525,7 +525,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.74, 78.74, 2560, 105.0 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 78.74, 78.74, 2560, 97.8 }
 
 /**
  * Default Max Feed Rate (mm/s)
